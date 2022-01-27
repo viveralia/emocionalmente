1ce888da79d57fa7e40d57faf4da7a684f78b5d5
Nicte Marín <nicte.tania@gmail.com>
2022-01-27 10:47:40 -0600

    feat: react Hook Form initial implementation
    #4

--- src/screens/OnBoardingProfileScreen.tsx
+++ src/screens/OnBoardingProfileScreen.tsx
@@ -1,94 +1,73 @@
 /* eslint-disable no-console */
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import {
-  Box,
-  Button,
-  Text,
-  Heading,
-  FormControl,
-  Input,
-  Stack,
-  WarningOutlineIcon,
-  Radio,
-  VStack,
-} from "native-base";
-import { FC, useState } from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { Box, Button, Text, Heading, FormControl, Input, Radio, VStack } from "native-base";
+import { FC } from "react";
+import { useForm, Controller } from "react-hook-form";
 
 import { RootStackParamList } from "../navigators/RootNavigator";
 import EmotionHappiness from "./../components/EmotionHappiness";
 import EmotionPlayfulValu from "./../components/EmotionPlayfulValu";
 import Logo from "./../components/Logo";
 
-type Inputs = {
-  name: string;
-};
+const OnBoardingProfileScreen: FC<NativeStackScreenProps<RootStackParamList>> = () => {
+  const {
+    control,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const avatar = watch("gender");
 
-const OnBoardingProfileScreen: FC<NativeStackScreenProps<RootStackParamList>> = ({
-  navigation,
-}) => {
-  const [avatar, setAvatar] = useState("LUP");
-  const [formData, setFormData] = useState({ name: "" });
-  const [errors, setErrors] = useState({});
-
-  const validate = () => {
-    if (formData.name === undefined) {
-      setErrors({
-        ...errors,
-        name: "El nombre es requerido",
-      });
-      return false;
-    }
-    return true;
-  };
-
-  const onSubmit = () => {
-    validate() ? console.log("Submitted") : console.log("Validation Failed");
-  };
+  // const onSubmit = (data) => {
+  //   console.log("submiting with ", data);
+  // };
 
   return (
-    <Box safeArea bg="brand" flex={1} alignItems="center" justifyContent="space-between">
+    <Box safeAreaTop bg="brand" flex={1} alignItems="center" justifyContent="space-between">
       <Logo />
-
-      <Button mt={2} onPress={navigation.goBack}>
-        Back
-      </Button>
       {avatar == "LUP" ? <EmotionHappiness /> : <EmotionPlayfulValu />}
-      <VStack width="100%" mx="3" background="white">
-        <FormControl isRequired isInvalid={"name" in errors}>
-          <Stack mx="4">
-            <Heading>Mi perfil</Heading>
-            <FormControl.Label>Mi nombre es:</FormControl.Label>
-            <Input
-              type="text"
-              defaultValue="Nombre"
-              placeholder="Nombre"
-              onChangeText={(value) => setFormData({ ...formData, name: value })}
-            />
-            <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-              El nombre es requerido
-            </FormControl.ErrorMessage>
-          </Stack>
+      <VStack space={4} width="100%" background="white" paddingY={8} paddingX={4} rounded="2xl">
+        <Heading>Mi perfil</Heading>
+        <FormControl>
+          <FormControl.Label>Mi nombre es:</FormControl.Label>
+          <Controller
+            control={control}
+            name="name"
+            render={({ field: { onChange, value, onBlur } }) => (
+              <Input
+                placeholder="Nombre"
+                value={value}
+                onBlur={onBlur}
+                onChangeText={(value) => onChange(value)}
+              />
+            )}
+            rules={{ min: 3, required: "El nombre es requerido" }}
+          />
+          <FormControl.ErrorMessage>{errors.name?.message}</FormControl.ErrorMessage>
+          <Text>Mi avatar será:</Text>
+          <Controller
+            control={control}
+            defaultValue="LUP"
+            name="gender"
+            render={({ field: { onChange, name, value } }) => (
+              <Radio.Group
+                name={name}
+                value={value}
+                onChange={(avatar) => {
+                  onChange(avatar);
+                }}
+              >
+                <Radio value="LUP" my={1}>
+                  Lup (hombre)
+                </Radio>
+                <Radio value="VALU" my={1}>
+                  Valu (mujer)
+                </Radio>
+              </Radio.Group>
+            )}
+          />
         </FormControl>
-        <Text>Mi avatar será:</Text>
-        <Radio.Group
-          name="myRadioGroup"
-          accessibilityLabel="favorite number"
-          value={avatar}
-          onChange={(nextValue) => {
-            setAvatar(nextValue);
-          }}
-        >
-          <Radio value="LUP" my={1}>
-            Lup (hombre)
-          </Radio>
-          <Radio value="VALU" my={1}>
-            Valu (mujer)
-          </Radio>
-        </Radio.Group>
-        <Button size={"lg"} onPress={onSubmit}>
-          Comenzar
-        </Button>
+        {/* <Button onPress={handleSubmit(onSubmit)}>Comenzar</Button> */}
       </VStack>
     </Box>
   );
