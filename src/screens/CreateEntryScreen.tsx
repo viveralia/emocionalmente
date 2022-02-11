a7f65cc11b6d5e8ba56f4a874d34c5245e854aba
Nicte Marín <nicte.tania@gmail.com>
2022-02-10 20:45:59 -0600

    feat(createentryscreen): initial layout
    Addition of form and initial layout
    
    #20

--- src/screens/CreateEntryScreen.tsx
+++ src/screens/CreateEntryScreen.tsx
@@ -1,45 +1,150 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack/lib/typescript/src/types";
-import { Box, Heading, Button, ScrollView } from "native-base";
-import { FC, useState } from "react";
-import { TextInput } from "react-native";
+import {
+  Box,
+  Heading,
+  Text,
+  Button,
+  ScrollView,
+  FormControl,
+  Input,
+  VStack,
+  FlatList,
+} from "native-base";
+import { FC } from "react";
+import { useForm, Controller, Resolver } from "react-hook-form";
 
 import { createEntries } from "../actions/entries.actions";
 import { useAppDispatch } from "../hooks/redux";
 import { EntriesStackParamList } from "../navigators/EntriesNavigator";
 
 const CreateEntryScreen: FC<NativeStackScreenProps<EntriesStackParamList>> = () => {
-  const [emotion, setEmotion] = useState("");
-  const [description, setDescription] = useState("");
-  const [bodyExpression, setBodyExpression] = useState("");
-
   const dispatch = useAppDispatch();
 
-  const handlePress = () => {
-    dispatch(createEntries({ emotion, description, bodyExpression }));
+  const onSubmit = (data: FormValues) => {
+    dispatch(
+      createEntries({
+        emotion: "feliz",
+        description: data.description,
+        bodyExpression: data.bodyExpression,
+      })
+    );
   };
 
+  type FormValues = {
+    description: string;
+    bodyExpression: string;
+    emotion: string;
+  };
+
+  const resolver: Resolver<FormValues> = async (values) => {
+    return {
+      values: values.description && values.bodyExpression ? values : {},
+      errors:
+        !values.description || !values.bodyExpression
+          ? {
+              description: {
+                type: "required",
+                message: "Una descripción es requerida",
+              },
+              bodyExpression: {
+                type: "required",
+                message: "La sensación fisica es requerida",
+              },
+            }
+          : {},
+    };
+  };
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({
+    resolver,
+    defaultValues: {
+      emotion: "feliz",
+      description: "",
+      bodyExpression: "",
+    },
+  });
+
+  const pets = [
+    { name: "momma", id: 1 },
+    { name: "momma1", id: 2 },
+    { name: "momma2", id: 3 },
+    { name: "momma3", id: 4 },
+    { name: "momma4", id: 5 },
+    { name: "momma5", id: 6 },
+    { name: "momma6", id: 7 },
+    { name: "momma7", id: 8 },
+    { name: "momma8", id: 9 },
+    { name: "momma9", id: 10 },
+  ];
+
   return (
-    <ScrollView>
-      <Box safeArea p={5} flex={1} alignItems="center" justifyContent="center">
-        <Heading>Crear nueva entrada</Heading>
-        <TextInput
-          placeholder="Emoción"
-          value={emotion}
-          onChangeText={(text) => setEmotion(text)}
+    <VStack flex={1} alignItems="center" justifyContent="space-between">
+      <ScrollView paddingX={4} paddingY={8}>
+        <Text mb="2">Hoy, 8 de enero de 2022</Text>
+        <Heading mb="4">¿Cómo me siento?</Heading>
+        {/* Emotion */}
+        <FlatList
+          numColumns={3}
+          data={pets}
+          renderItem={({ item }) => (
+            <Box marginX={2} marginY={2} width="20" height="20" bg="brand">
+              <Text>{item.name}</Text>
+            </Box>
+          )}
         />
-        <TextInput
-          placeholder="Descripción"
-          value={description}
-          onChangeText={(text) => setDescription(text)}
-        />
-        <TextInput
-          placeholder="Expresion corporal"
-          value={bodyExpression}
-          onChangeText={(text) => setBodyExpression(text)}
-        />
-        <Button onPress={handlePress}>Agregar</Button>
-      </Box>
-    </ScrollView>
+        {/* Description */}
+        <FormControl isInvalid={"description" in errors}>
+          <FormControl.Label>
+            <Heading size="xs">¿Qué me hizo sentir así?</Heading>
+          </FormControl.Label>
+
+          <Controller
+            control={control}
+            name="description"
+            render={({ field: { onChange, onBlur, value } }) => (
+              <Input
+                borderColor="white"
+                bg="white"
+                placeholder="Acciones o pensamientos que noté."
+                value={value}
+                onBlur={onBlur}
+                onChangeText={(value) => onChange(value)}
+                height="100"
+              />
+            )}
+          />
+          <FormControl.ErrorMessage>{errors.description?.message}</FormControl.ErrorMessage>
+        </FormControl>
+        {/* Body Expression */}
+        <FormControl isInvalid={"bodyExpression" in errors}>
+          <FormControl.Label>
+            <Heading size="xs">¿Cómo lo expresa mi cuerpo?</Heading>
+          </FormControl.Label>
+          <Controller
+            control={control}
+            name="bodyExpression"
+            render={({ field: { onChange, onBlur, value } }) => (
+              <Input
+                borderColor="white"
+                bg="white"
+                placeholder="La sensación física que estoy experimentando."
+                value={value}
+                onBlur={onBlur}
+                onChangeText={(value) => onChange(value)}
+              />
+            )}
+          />
+          <FormControl.ErrorMessage>{errors.bodyExpression?.message}</FormControl.ErrorMessage>
+        </FormControl>
+        <Button mb="4" mx={4} onPress={handleSubmit(onSubmit)}>
+          Guardar
+        </Button>
+      </ScrollView>
+    </VStack>
   );
 };
 
