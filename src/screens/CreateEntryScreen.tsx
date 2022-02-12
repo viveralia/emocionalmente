de127b5f5fe260069a914101758fc686babf423b
Nicte Marín <nicte.tania@gmail.com>
2022-02-12 17:50:14 -0600

    feat(createentryscreen): add Flatlist to display emotions depending on avatar
    #20

--- src/screens/CreateEntryScreen.tsx
+++ src/screens/CreateEntryScreen.tsx
@@ -1,24 +1,17 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack/lib/typescript/src/types";
-import {
-  Box,
-  Heading,
-  Text,
-  Button,
-  ScrollView,
-  FormControl,
-  Input,
-  VStack,
-  FlatList,
-} from "native-base";
+import { Heading, Text, Button, ScrollView, FormControl, Input, FlatList } from "native-base";
 import { FC } from "react";
 import { useForm, Controller, Resolver } from "react-hook-form";
+import { TouchableOpacity } from "react-native";
 
 import { createEntries } from "../actions/entries.actions";
-import { useAppDispatch } from "../hooks/redux";
+import { useAppDispatch, useAppSelector } from "../hooks/redux";
 import { EntriesStackParamList } from "../navigators/EntriesNavigator";
+import { getEmotionsByAvatar } from "../utils/emotions";
 
 const CreateEntryScreen: FC<NativeStackScreenProps<EntriesStackParamList>> = () => {
   const dispatch = useAppDispatch();
+  const { profile } = useAppSelector((state) => state.user);
 
   const onSubmit = (data: FormValues) => {
     dispatch(
@@ -68,83 +61,69 @@ const CreateEntryScreen: FC<NativeStackScreenProps<EntriesStackParamList>> = () 
     },
   });
 
-  const pets = [
-    { name: "momma", id: 1 },
-    { name: "momma1", id: 2 },
-    { name: "momma2", id: 3 },
-    { name: "momma3", id: 4 },
-    { name: "momma4", id: 5 },
-    { name: "momma5", id: 6 },
-    { name: "momma6", id: 7 },
-    { name: "momma7", id: 8 },
-    { name: "momma8", id: 9 },
-    { name: "momma9", id: 10 },
-  ];
-
   return (
-    <VStack flex={1} alignItems="center" justifyContent="space-between">
-      <ScrollView paddingX={4} paddingY={8}>
-        <Text mb="2">Hoy, 8 de enero de 2022</Text>
-        <Heading mb="4">¿Cómo me siento?</Heading>
-        {/* Emotion */}
-        <FlatList
-          numColumns={3}
-          data={pets}
-          renderItem={({ item }) => (
-            <Box marginX={2} marginY={2} width="20" height="20" bg="brand">
-              <Text>{item.name}</Text>
-            </Box>
+    <ScrollView flex={1}>
+      <Text mb="2">Hoy, 8 de enero de 2022</Text>
+      <Heading mb="4">¿Cómo me siento?</Heading>
+      {/* Emotion */}
+      <FlatList
+        numColumns={4}
+        data={getEmotionsByAvatar(profile!.avatar)}
+        renderItem={({ item: { EmotionSvg, name } }) => (
+          <TouchableOpacity>
+            <EmotionSvg width="80" height="80" />
+            <Text>{name}</Text>
+          </TouchableOpacity>
+        )}
+      />
+      {/* Description */}
+      <FormControl isInvalid={"description" in errors}>
+        <FormControl.Label>
+          <Heading size="xs">¿Qué me hizo sentir así?</Heading>
+        </FormControl.Label>
+
+        <Controller
+          control={control}
+          name="description"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <Input
+              borderColor="white"
+              bg="white"
+              placeholder="Acciones o pensamientos que noté."
+              value={value}
+              onBlur={onBlur}
+              onChangeText={(value) => onChange(value)}
+              height="100"
+            />
           )}
         />
-        {/* Description */}
-        <FormControl isInvalid={"description" in errors}>
-          <FormControl.Label>
-            <Heading size="xs">¿Qué me hizo sentir así?</Heading>
-          </FormControl.Label>
-
-          <Controller
-            control={control}
-            name="description"
-            render={({ field: { onChange, onBlur, value } }) => (
-              <Input
-                borderColor="white"
-                bg="white"
-                placeholder="Acciones o pensamientos que noté."
-                value={value}
-                onBlur={onBlur}
-                onChangeText={(value) => onChange(value)}
-                height="100"
-              />
-            )}
-          />
-          <FormControl.ErrorMessage>{errors.description?.message}</FormControl.ErrorMessage>
-        </FormControl>
-        {/* Body Expression */}
-        <FormControl isInvalid={"bodyExpression" in errors}>
-          <FormControl.Label>
-            <Heading size="xs">¿Cómo lo expresa mi cuerpo?</Heading>
-          </FormControl.Label>
-          <Controller
-            control={control}
-            name="bodyExpression"
-            render={({ field: { onChange, onBlur, value } }) => (
-              <Input
-                borderColor="white"
-                bg="white"
-                placeholder="La sensación física que estoy experimentando."
-                value={value}
-                onBlur={onBlur}
-                onChangeText={(value) => onChange(value)}
-              />
-            )}
-          />
-          <FormControl.ErrorMessage>{errors.bodyExpression?.message}</FormControl.ErrorMessage>
-        </FormControl>
-        <Button mb="4" mx={4} onPress={handleSubmit(onSubmit)}>
-          Guardar
-        </Button>
-      </ScrollView>
-    </VStack>
+        <FormControl.ErrorMessage>{errors.description?.message}</FormControl.ErrorMessage>
+      </FormControl>
+      {/* Body Expression */}
+      <FormControl isInvalid={"bodyExpression" in errors}>
+        <FormControl.Label>
+          <Heading size="xs">¿Cómo lo expresa mi cuerpo?</Heading>
+        </FormControl.Label>
+        <Controller
+          control={control}
+          name="bodyExpression"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <Input
+              borderColor="white"
+              bg="white"
+              placeholder="La sensación física que estoy experimentando."
+              value={value}
+              onBlur={onBlur}
+              onChangeText={(value) => onChange(value)}
+            />
+          )}
+        />
+        <FormControl.ErrorMessage>{errors.bodyExpression?.message}</FormControl.ErrorMessage>
+      </FormControl>
+      <Button mb="4" mx={4} onPress={handleSubmit(onSubmit)}>
+        Guardar
+      </Button>
+    </ScrollView>
   );
 };
 
